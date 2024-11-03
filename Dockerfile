8466064a1c94ceb7e7054a438c8bd0ac8e3504b5
adisve <adis.veletanlic@gmail.com>
2024-11-03 14:10:37 +0100

    Rename app in express

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,3 @@ RUN npm install
 COPY ./ /app/
 ARG configuration=production
 RUN npm run build -- --output-path=./dist/out --configuration $configuration
-
-FROM nginx:1.26
-
-RUN rm -rf /usr/share/nginx/html/*
-COPY --from=build-stage /app/dist/out/ /usr/share/nginx/html
-
-RUN sed -i -e 's/$PORT/'"$PORT"'/g' config/nginx.conf.erb
-RUN touch /tmp/app-initialized
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/tumble-frontend'));
+app.use(express.static(__dirname + '/dist/out'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/tumble-frontend/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/out/index.html'));
 });
 app.listen(process.env.PORT || 8080);
